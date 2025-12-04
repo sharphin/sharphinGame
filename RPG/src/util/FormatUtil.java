730080644382281da832a635fb5aa8e6fe42a4d0
hiramatsu <yut-hiramatsu@brexa.com>
2025-12-04 15:20:39 +0900

    gradleに変更

--- RPG/src/util/FormatUtil.java
+++
@@ -1,7 +0,0 @@
-package util;
-
-import java.time.format.DateTimeFormatter;
-
-public class FormatUtil {
-    public static final DateTimeFormatter format1 = DateTimeFormatter.ofPattern("yyyy年MM月dd日 HH:mm:ss");
-}
